b69b4063cd20cf09c70000d32049e5967a5bd135
professorsugoi <emosock@live.com>
2023-04-01 22:46:45 -0400

    replace default branding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
@@ -60,24 +59,24 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			// Replace with your project's social card
-			image: 'img/docusaurus-social-card.jpg',
+			image: 'img/logo.svg',
 			navbar: {
-				title: 'My Site',
+				title: 'DEVILISH WIKI',
 				logo: {
-					alt: 'My Site Logo',
+					alt: 'Devilish Logo',
 					src: 'img/logo.svg',
 				},
 				items: [
 					{
 						type: 'docSidebar',
-						sidebarId: 'tutorialSidebar',
+						sidebarId: 'wikiSidebar',
 						position: 'left',
-						label: 'Tutorial',
+						label: 'Wiki',
 					},
 					{ to: '/blog', label: 'Blog', position: 'left' },
 					{
-						href: 'https://github.com/facebook/docusaurus',
-						label: 'GitHub',
+						href: 'https://devilish.wtf',
+						label: 'Devilish.wtf',
 						position: 'right',
 					},
 				],
@@ -86,28 +85,15 @@ const config = {
 				style: 'dark',
 				links: [
 					{
-						title: 'Docs',
-						items: [
-							{
-								label: 'Tutorial',
-								to: '/docs/intro',
-							},
-						],
-					},
-					{
 						title: 'Community',
 						items: [
 							{
-								label: 'Stack Overflow',
-								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-							},
-							{
 								label: 'Discord',
-								href: 'https://discordapp.com/invite/docusaurus',
+								href: 'https://discord.gg/WcxfnWNdUB',
 							},
 							{
 								label: 'Twitter',
-								href: 'https://twitter.com/docusaurus',
+								href: 'https://twitter.com/666sugoi',
 							},
 						],
 					},
@@ -120,16 +106,15 @@ const config = {
 							},
 							{
 								label: 'GitHub',
-								href: 'https://github.com/facebook/docusaurus',
+								href: 'https://github.com/professorsugoi/devilish-wiki',
 							},
 						],
 					},
 				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+				copyright: `© ${new Date().getFullYear()} DEVILISH. Built with ♥`,
 			},
 			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
+				theme: darkCodeTheme,
 			},
 		}),
 };
